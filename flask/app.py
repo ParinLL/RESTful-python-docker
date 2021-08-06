1a4601a09a5336dd1ed5e6c6b14b6e51eeb749db
Parin Lai <admin@parinweb.cc>
2021-08-06 14:38:26 +0800

    feat: Restful API almost done

--- flask/app.py
+++ flask/app.py
@@ -15,44 +15,88 @@ USERS = {
             }
 }
 
-PORT = os.getenv('FLASK_PORT','8080')
+PORT = os.getenv('FLASK_PORT','80')
 
 def abort_if_user_doesnt_exist(USER_id):
     if USER_id not in USERS:
         abort(404, message="USER {} doesn't exist".format(USER_id))
 
 parser = reqparse.RequestParser()
-parser.add_argument('name')
+parser.add_argument('name', type=str, required=True,help="Name cannot be blank!")
+parser.add_argument('job_title', type=str)
+parser.add_argument('email', type=str)
+parser.add_argument('mobile', type=str)
 
 class User(Resource):
     def get(self, USER_id):
         abort_if_user_doesnt_exist(USER_id)
-        return USERS[USER_id]
+        resp=USERS[USER_id]
+        respond=jsonify(resp)
+        respond.status_code=200
+        return respond
+        # return USERS[USER_id], 200
 
     def delete(self, USER_id):
         abort_if_user_doesnt_exist(USER_id)
+        user_num=USERS[USER_id]
         del USERS[USER_id]
-        return '', 204
+        # return '', 204
+        # resp=USERS[USER_id]
+        # respond=jsonify(resp)
+        # respond.status_code=204
+        return "User has been deleted",204
 
     def put(self, USER_id):
+        abort_if_user_doesnt_exist(USER_id)
         args = parser.parse_args()
-        name = {"name": args["name"]}
-        USERS[USER_id] = name
-        return name, 201
+        user = {
+            USER_id:{
+            'name': args['name'],
+            'job_title': args['job_title'],
+            'communicate_information':{
+               'email':  args['email'],
+               'mobile': args['mobile']
+                }
+                    }
+        }
+        USERS.update(user)
+        respond= jsonify(user)
+        respond.status_code=201
+        print(respond)
+        return respond
+        #name = {"name": args["name"]}
+        #USERS[USER_id] = name
+        #return USERS[USER_id], 201
 
 class UserList(Resource):
     def get(self):
-        return USERS
+        # respond=jsonify(USERS)
+        # respond.status_code=200        
+        return USERS, 200
 
     def post(self):
         args = parser.parse_args()
         USER_id = int(max(USERS.keys()).lstrip('user')) + 1
         USER_id = 'user%i' % USER_id
-        USERS[USER_id] = {'name': args['name']}
-        return USERS[USER_id], 201
+        user = {
+            USER_id:{
+            'name': args['name'],
+            'job_title': args['job_title'],
+            'communicate_information':{
+               'email':  args['email'],
+               'mobile': args['mobile']
+                }
+                    }
+        }
+        USERS.update(user)
+        respond= jsonify(user)
+        respond.status_code=201
+        print(respond)
+        return respond
+        # return USERS[USER_id], "USER {} has been created or updated".format(USER_id)
 
 api.add_resource(UserList, '/users', '/users/')
-api.add_resource(User, '/users/<USER_id>')
+api.add_resource(User, '/users/<USER_id>', '/users/<USER_id>/')
 
 # @app.route('/')
 # def hello_world():
